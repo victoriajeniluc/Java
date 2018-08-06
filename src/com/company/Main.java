09a332928f2c310f3a3d94260c3b4162ffd23435
Victoria Luc <victoriajeniluc@gmail.com>
2018-08-06 01:12:38 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+
+public class Main {
+    public static void main(String[] args) {
+        /* This is how you print out Hello World*/
+        System.out.println("Hello World");
+        System.out.println("Hello World Again");
+    }
+
+}
