eaa2a4d023f36d17ec82bbcbcec776fb3ee8f10e
Victoria Luc <victoriajeniluc@gmail.com>
2018-08-06 01:22:57 -0500

    Specify the comment on System.out.println()

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,7 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        /* This is how you print out Hello World*/
-        /* will this show up? */
+        /* This is how you print out Hello World - refer to Sublime Text for info on System.out.println()*/
         System.out.println("Hello World");
         System.out.println("Hello World Again");
     }
